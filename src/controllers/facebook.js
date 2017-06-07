fba9168ee72ce8098e8262ba814d1dd27e1aaf9c
Alejandro Ramirez <alex.ramirez@wizeline.com>
2017-06-07 16:48:50 -0500

    Echo bot

--- src/controllers/facebook.js
+++ src/controllers/facebook.js
@@ -26,19 +26,29 @@ const receivedMessage = event => {
   const text = event.message.text
 
   // Mapped entities
-  const entities = wit.getEntities(text)
+  // const entities = wit.getEntities(text)
 
+  sendTextMessage(senderId, text)
 }
 
 const sendTextMessage = (recipientId, messageText) => {
+  const message = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  }
 
+  callSendAPI(message)
 }
 
 const callSendAPI = message => {
   requestPromise({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {
-      access_token: process.env.PAGE_ACCESS_TOKEN
+      access_token: process.env.FB_PAGE_ACCESS_TOKEN
     },
     method: 'POST',
     json: message
@@ -57,7 +67,32 @@ const callSendAPI = message => {
 }
 
 const postWebhook = (req, res) => {
+  const data = req.body
 
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(entry => {
+      const pageID = entry.id
+      const timeOfEvent = entry.time
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(event => {
+        if (event.message) {
+          receivedMessage(event)
+        } else {
+          console.log("Webhook received unknown event: ", event)
+        }
+      })
+    })
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let Facebook know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and they will keep trying to resend.
+    res.sendStatus(200)
+  }
 }
 
 module.exports = {
